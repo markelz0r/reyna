671f8812f9e77c5594a50809671a50ac0d33ea36
Andrew Odintsov <markel33@MBP-Andrew-4.home>
2016-10-01 15:36:12 +0100

    added files

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+//var forever = require('forever-monitor');
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res) {
+     res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/gallery', function (req, res) {
+     res.sendFile(path.join(__dirname+'/gallery.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
