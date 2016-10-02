6e95eb055cb9a0b73011a5072fb246434552df47
Andrew Odintsov <markel33@MBP-Andrew-4.home>
2016-10-02 21:22:44 +0100

    added checkout page and post requests handling

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
+var bodyParser     =        require("body-parser");
 var app = express();
 var path = require('path');
 var cookieSession = require('cookie-session')
 //var forever = require('forever-monitor');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 
@@ -16,8 +19,9 @@ app.get('/gallery', function (req, res) {
      res.sendFile(path.join(__dirname+'/gallery.html'));
 });
 
-app.get('/checkout', function (req, res) {
+app.post('/checkout', function (req, res) {
      res.sendFile(path.join(__dirname+'/checkout.html'));
+     console.log('current order is:'+req.body);
 });
 
 
